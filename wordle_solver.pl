ff3c8eaa8826abbca1f7443fa2eb7e6c9cbdbf3d
disha pandurangi <81485873+dishaap@users.noreply.github.com>
2023-04-02 13:44:40 -0700

    added notcontain

--- wordle_solver.pl
+++ wordle_solver.pl
@@ -8,3 +8,8 @@ five_letter_words(X) :- s(_, _, X, _, _, _), string_length(X, 5).
 % this is a letter you KNOW is in the word at that particular index
 % green gives all 5 letter words that contain that letter at that particular index
 green(Letter, Index, Word) :- five_letter_words(Word), sub_string(Word, Index, 1, _, Letter1), Letter1 == Letter.
+
+% notcontain takes a list of letters and returns all the five-letter words that 
+% do not contain those letters at any position. these are letters we
+% know don't appear in any position in the word.
+notcontain(Letters,Word) :- five_letter_words(Word), forall(member(Letter,Letters),not(sub_string(Word, _, 1, _, Letter))).
